2f26c590023b571587694c70f52e38f200276e84
deadcheat <deadcheat@users.noreply.github.com>
2018-07-30 01:08:10 +0900

    start implementing for #1

---
+++ values/consts/values.go
@@ -0,0 +1,20 @@
+package consts
+
+const (
+	// RFC2822 format of time
+	RFC2822 = "Mon Jan 02 15:04:05 -0700 2006"
+)
+
+const (
+	// ParamKeyService key string for param
+	ParamKeyService = "service"
+	// ParamKeyRenew key string for param
+	ParamKeyRenew = "renew"
+	// ParamKeyGateway key string for param
+	ParamKeyGateway = "gateway"
+)
+
+const (
+	// CookieKeyTGT key string for cookie
+	CookieKeyTGT = "tgt"
+)
