64c3a5ec3f0279563115c94cc534b24bd651486d
sasakim <sasakim@istyle.co.jp>
2018-08-02 21:28:03 +0900

    fixed to return *url.URL from NormarizeURL

--- helpers/service/helper_test.go
+++ helpers/service/helper_test.go
@@ -1,6 +1,10 @@
 package service
 
-import "testing"
+import (
+	"net/url"
+	"reflect"
+	"testing"
+)
 
 func TestNormalizeURL(t *testing.T) {
 	type args struct {
@@ -9,50 +13,64 @@ func TestNormalizeURL(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		want    string
+		want    *url.URL
 		wantErr bool
 	}{
 		// TODO: Add test cases.
 		{
-			name:    "remove trailing ? for http://example.com?",
-			args:    args{u: "http://example.com?"},
-			want:    "http://example.com",
+			name: "remove trailing ? for http://example.com?",
+			args: args{u: "http://example.com?"},
+			want: &url.URL{
+				Scheme: "http",
+				Host:   "example.com",
+			},
 			wantErr: false,
 		},
 		{
-			name:    "remove trailing / for http://example.com/",
-			args:    args{u: "http://example.com/"},
-			want:    "http://example.com",
+			name: "remove trailing / for http://example.com/",
+			args: args{u: "http://example.com/"},
+			want: &url.URL{
+				Scheme: "http",
+				Host:   "example.com",
+			},
 			wantErr: false,
 		},
 		{
-			name:    "remove trailingslash last '&' in query params for \"http://example.com/?ticket=12345&\"",
-			args:    args{u: "http://example.com/?ticket=12345&"},
-			want:    "http://example.com?ticket=12345",
+			name: "remove trailingslash last '&' in query params for \"http://example.com/?ticket=12345&\"",
+			args: args{u: "http://example.com/?ticket=12345&"},
+			want: &url.URL{
+				Scheme:   "http",
+				Host:     "example.com",
+				RawQuery: "ticket=12345",
+			},
 			wantErr: false,
 		},
 		{
-			name:    "unexcape query and url for \"http%3A%2F%2Fexample.com%2F%3Fticket%3D12345\"",
-			args:    args{u: "http%3A%2F%2Fexample.com%2F%3Fticket%3D12345"},
-			want:    "http://example.com?ticket=12345",
+			name: "unexcape query and url for \"http%3A%2F%2Fexample.com%2F%3Fticket%3D12345\"",
+			args: args{u: "http%3A%2F%2Fexample.com%2F%3Fticket%3D12345"},
+			want: &url.URL{
+				Scheme:   "http",
+				Host:     "example.com",
+				RawQuery: "ticket=12345",
+			},
 			wantErr: false,
 		},
 		{
 			name:    "return escape error for \"%\"",
 			args:    args{u: "http://%"},
-			want:    "",
+			want:    nil,
 			wantErr: true,
 		},
 		{
 			name:    "parse error",
 			args:    args{u: "://"},
-			want:    "",
+			want:    nil,
 			wantErr: true,
 		},
 		{
 			name:    "do not parse when blank",
 			args:    args{u: "  "},
-			want:    "",
+			want:    nil,
 			wantErr: false,
 		},
 	}
@@ -60,11 +78,11 @@ func TestNormalizeURL(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := NormalizeURL(tt.args.u)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("NormalizeURL() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("NormalizeURL() error = %#+v, wantErr %#+v", err, tt.wantErr)
 				return
 			}
-			if got != tt.want {
-				t.Errorf("NormalizeURL() = %v, want %v", got, tt.want)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NormalizeURL() = %#+v, want %#+v", got, tt.want)
 			}
 		})
 	}
