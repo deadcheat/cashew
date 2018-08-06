6d933aa48a674dc848e25a0db5fc2a334080867b
sasakim <sasakim@istyle.co.jp>
2018-08-06 19:58:00 +0900

    adde make main.go tidy

--- cmd/cashew/main.go
+++ cmd/cashew/main.go
@@ -10,6 +10,7 @@ import (
 )
 
 func main() {
+	// load global setting
 	l := foundation.GlobalSettingLoader()
 	configFile := "config.yml"
 	_, err := l.Load(configFile)
@@ -17,9 +18,12 @@ func main() {
 		log.Fatalf("failed to load config file %s \n", configFile)
 	}
 
+	// create usecase, repository, deliver and mount them
 	r := mux.NewRouter()
 	d := dh.New(r)
 	d.Mount()
+
+	// start cas server
 	log.Println("start cas server")
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
