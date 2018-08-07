25d1a7c46a33b4eddd4fc9a0bbfb70cd16016f9d
deadcheat <deadcheat@users.noreply.github.com>
2018-08-08 00:41:46 +0900

    added TimeWrapper

--- timer/time.go
+++ timer/time.go
@@ -2,7 +2,22 @@ package timer
 
 import "time"
 
-// Clock is a wrapper for time
-type Clock interface {
+// Local Local variable for TimeWrapper
+var Local TimeWrapper
+
+func init() {
+	Local = new(LocalTimer)
+}
+
+// TimeWrapper is a wrapper for time
+type TimeWrapper interface {
 	Now() time.Time
+}
+
+// LocalTimer implement TimeWrapper
+type LocalTimer struct{}
+
+// Now return time.Now() simply
+func (l *LocalTimer) Now() time.Time {
+	return time.Now()
 }
