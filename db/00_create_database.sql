2b825de7a86dcaa3037201a399946305864769a9
deadcheat <deadcheat@users.noreply.github.com>
2018-08-13 23:07:39 +0900

    added migrate sql

--- db/00_create_database.sql
+++ db/00_create_database.sql
@@ -1,3 +1,3 @@
-CREATE database casdb;
+CREATE DATABASE casdb CHARACTER SET utf8mb4 collate utf8mb4_bin;
 CREATE USER casuser IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT ALL PRIVILEGES on casdb.* to 'casuser'@'%';
--- db/migrations/20180813172457_AddInitialTables.sql
+++ db/migrations/20180813172457_AddInitialTables.sql
@@ -1,7 +1,109 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
+-- tickets
+CREATE TABLE tickets
+(
+    id varchar(256) PRIMARY KEY NOT NULL COMMENT 'ticket id',
+    client_hostname varchar(255) NOT NULL COMMENT 'hostname of client',
+    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'created datetime'
+)
+DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_bin
+COMMENT 'all tickets are not classified by their types';
+CREATE UNIQUE INDEX tickets_id_uindex ON tickets (id);
 
+-- tickets_service
+CREATE TABLE ticket_service
+(
+    ticket_id varchar(256) NOT NULL COMMENT 'ticket id',
+    service text(2083) NOT NULL COMMENT 'service url that will be or has been granted',
+    created_at datetime DEFAULT current_timestamp NOT NULL COMMENT 'created datetime',
+    CONSTRAINT fk_ticket_service_tickets FOREIGN KEY (ticket_id) REFERENCES tickets (id)
+)
+DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_bin
+COMMENT 'service url for ticket that issued as service-ticket';
+-- service is too long to create index
+
+-- ticket_type
+CREATE TABLE ticket_type
+(
+    ticket_id varchar(256) NOT NULL COMMENT 'ticket id',
+    type varchar(50) NOT NULL COMMENT 'ticket type login/service/proxy/proxy granting/ticket granting',
+    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'created datetime',
+    CONSTRAINT fk_ticket_type_tickets FOREIGN KEY (ticket_id) REFERENCES tickets (id)
+)
+DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_bin
+COMMENT 'type for ticket';
+CREATE INDEX idx_ticket_type_type ON ticket_type (type);
+
+-- ticket_username
+CREATE TABLE ticket_username
+(
+    ticket_id varchar(256) NOT NULL COMMENT 'ticket id',
+    username varchar(255) NOT NULL COMMENT '',
+    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'created datetime',
+    CONSTRAINT fk_ticket_username_tickets FOREIGN KEY (ticket_id) REFERENCES tickets (id)
+)
+DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_bin
+COMMENT 'username for ticket such as proxy grainting ticket and service ticket';
+CREATE INDEX idx_ticket_username_username ON ticket_username (username);
+
+-- ticket_expires
+CREATE TABLE ticket_expires
+(
+    ticket_id varchar(256) NOT NULL COMMENT 'ticket id',
+    expires_at datetime NOT NULL COMMENT '',
+    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'created datetime',
+    CONSTRAINT fk_ticket_expires_tickets FOREIGN KEY (ticket_id) REFERENCES tickets (id)
+)
+DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_bin
+COMMENT 'expire datetime for ticket';
+CREATE INDEX idx_ticket_expires_expires_at ON ticket_expires (expires_at);
+
+-- ticket_iou
+CREATE TABLE ticket_iou
+(
+    ticket_id varchar(256) NOT NULL COMMENT 'ticket id',
+    iou varchar(256) NOT NULL COMMENT '',
+    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'created datetime',
+    CONSTRAINT fk_ticket_iou_tickets FOREIGN KEY (ticket_id) REFERENCES tickets (id)
+)
+DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_bin
+COMMENT 'iou for ticket. this will be used when ticket is proxy granting ticket';
+CREATE INDEX idx_ticket_iou_iou ON ticket_iou (iou);
+
+-- ticket_extra_attribute
+CREATE TABLE ticket_extra_attribute
+(
+    ticket_id varchar(256) NOT NULL COMMENT 'ticket id',
+    extra_attribute json NOT NULL COMMENT 'extra attributes serialized to json',
+    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'created datetime',
+    CONSTRAINT fk_ticket_extra_attribute_tickets FOREIGN KEY (ticket_id) REFERENCES tickets (id)
+)
+DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_bin
+COMMENT '';
+
+-- ticket_grant_ticket
+CREATE TABLE ticket_grant_ticket
+(
+    id varchar(256) PRIMARY KEY NOT NULL COMMENT 'granted id',
+    source_ticket_id varchar(256) NOT NULL COMMENT 'ticket id has granted',
+    destination_ticket_id varchar(256) NOT NULL COMMENT 'ticket id has been granted',
+    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'created datetime',
+    CONSTRAINT fk_ticket_grant_ticket_tickets_as_source FOREIGN KEY (source_ticket_id) REFERENCES tickets (id),
+    CONSTRAINT fk_ticket_grant_ticket_tickets_as_destination FOREIGN KEY (destination_ticket_id) REFERENCES tickets (id)
+)
+DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_bin
+COMMENT 'ticket id that grant other ticket';
+CREATE UNIQUE INDEX ticket_grant_ticket_id_uindex ON ticket_grant_ticket (id);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE ticket_grant_ticket;
+DROP TABLE ticket_extra_attribute;
+DROP TABLE ticket_iou;
+DROP TABLE ticket_expires;
+DROP TABLE ticket_username;
+DROP TABLE ticket_type;
+DROP TABLE ticket_service;
+DROP TABLE tickets;
