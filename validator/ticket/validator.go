55a8766b1575726c1c40dcc66d83c0868780ca31
deadcheat <deadcheat@users.noreply.github.com>
2019-01-18 17:16:38 +0900

    fixed validator

--- validator/ticket/validator.go
+++ validator/ticket/validator.go
@@ -24,21 +24,35 @@ func New() Validator {
 
 // Check ticket validateion
 func (p *v) Validate(t *cashew.Ticket) error {
+	if t == nil {
+		return errors.ErrTicketNotFound
+	}
 	now := timer.Local.Now()
 	// tickets will be expired in granting-default-expire-time(default: 8 hour) from the last time ticket has been referenced
 	// this is a rule for proxy granting ticket and ticket granting ticket
 	switch t.Type {
 	case cashew.TicketTypeLogin:
-		if t.LastReferencedAt != nil && now.After(t.LastReferencedAt.Add(time.Duration(foundation.App().LoginTicketExpire)*time.Second)) {
+		e := foundation.App().LoginTicketExpire
+		if now.After(t.CreatedAt.Add(time.Duration(e) * time.Second)) {
 			return errors.ErrTicketHasBeenExpired
 		}
 	case cashew.TicketTypeProxyGranting, cashew.TicketTypeTicketGranting:
-		if t.LastReferencedAt != nil && now.After(t.LastReferencedAt.Add(time.Duration(foundation.App().GrantingDefaultExpire)*time.Second)) {
+		e := foundation.App().GrantingDefaultExpire
+		// zero as infinity
+		if e == 0 {
+			return nil
+		}
+		if t.LastReferencedAt != nil && now.After(t.LastReferencedAt.Add(time.Duration(e)*time.Second)) {
 			return errors.ErrTicketHasBeenExpired
 		}
 	default:
+		e := foundation.App().GrantingHardTimeout
+		// zero as infinity
+		if e == 0 {
+			return nil
+		}
 		// hard time out will happen in default-hard-expire time(default 2hour) from ticket has been created
-		if now.After(t.CreatedAt.Add(time.Duration(foundation.App().GrantingHardTimeout) * time.Second)) {
+		if now.After(t.CreatedAt.Add(time.Duration(e) * time.Second)) {
 			return errors.ErrHardTimeoutTicket
 		}
 	}
